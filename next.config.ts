0e9a81a3337349d19e3e60c4071c81c4b825da9b
Netz <49691921+Netz00@users.noreply.github.com>
2025-05-10 20:06:12 +0000

    Add obfuscator

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from 'next';
+import WebpackObfuscator from 'webpack-obfuscator';
+import obfuscatorOptions from './obfuscator.config';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins!.push(
+        new WebpackObfuscator(obfuscatorOptions, ['runtime~*.js'])
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ obfuscator.config.ts
@@ -0,0 +1,38 @@
+import { WebpackObfuscatorOptions } from 'webpack-obfuscator/dist/plugin';
+
+const obfuscatorOptions: WebpackObfuscatorOptions = {
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 1,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 1,
+  debugProtection: true,
+  debugProtectionInterval: 4000,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: 'hexadecimal',
+  log: false,
+  numbersToExpressions: true,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true,
+  splitStrings: true,
+  splitStringsChunkLength: 5,
+  stringArray: true,
+  stringArrayCallsTransform: true,
+  stringArrayEncoding: ['rc4'],
+  stringArrayIndexShift: true,
+  stringArrayRotate: true,
+  stringArrayShuffle: true,
+  stringArrayWrappersCount: 5,
+  stringArrayWrappersChainedCalls: true,
+  stringArrayWrappersParametersMaxCount: 5,
+  stringArrayWrappersType: 'function',
+  stringArrayThreshold: 1,
+  transformObjectKeys: true,
+  unicodeEscapeSequence: false,
+  //   domainLock: ['YourDomain.com'],
+  //   domainLockRedirectUrl:
+  //     'RedirectUrl.com',
+};
+
+export default obfuscatorOptions;
